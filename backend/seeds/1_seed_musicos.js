4be47c9882695754ecff2480b400e4d37f0d13ab
marticoronel <martinacoronelpiatti@gmail.com>
2024-03-01 20:25:07 -0300

    correccion extensión fotos cantantes

--- backend/seeds/1_seed_musicos.js
+++ backend/seeds/1_seed_musicos.js
@@ -3,16 +3,16 @@ exports.seed = function (knex) {
     .del()
     .then(function () {
       return knex("musicos").insert([
-        { nombre_cantante: "buitres", tapa_cantante: "buitrez.png" },
-        { nombre_cantante: "karibe con k", tapa_cantante: "karibeconk.png" },
-        { nombre_cantante: "ruben rada", tapa_cantante: "rubenrada.png" },
-        { nombre_cantante: "shakira", tapa_cantante: "shakira.png" },
-        { nombre_cantante: "rosalia", tapa_cantante: "rosalia.png" },
-        { nombre_cantante: "luana", tapa_cantante: "buitrez.png" },
-        { nombre_cantante: "rolling stones", tapa_cantante: "rollingstone.png" },
-        { nombre_cantante: "silk sonic", tapa_cantante: "silksonic.png" },
-        { nombre_cantante: "tribalistas", tapa_cantante: "tribalistas.png" },
-        { nombre_cantante: "larbanois-carrero", tapa_cantante: "larbanois.png" },
+        { nombre_cantante: "buitres", tapa_cantante: "buitrez.jpg" },
+        { nombre_cantante: "karibe con k", tapa_cantante: "karibeconk.jpg" },
+        { nombre_cantante: "ruben rada", tapa_cantante: "rubenrada.jpg" },
+        { nombre_cantante: "shakira", tapa_cantante: "shakira.jpg" },
+        { nombre_cantante: "rosalia", tapa_cantante: "rosalia.jpg" },
+        { nombre_cantante: "luana", tapa_cantante: "buitrez.jpg" },
+        { nombre_cantante: "rolling stones", tapa_cantante: "rollingstone.jpg" },
+        { nombre_cantante: "silk sonic", tapa_cantante: "silksonic.jpg" },
+        { nombre_cantante: "tribalistas", tapa_cantante: "tribalistas.jpg" },
+        { nombre_cantante: "larbanois-carrero", tapa_cantante: "larbanois.jpg" },
       ]);
     });
 };
