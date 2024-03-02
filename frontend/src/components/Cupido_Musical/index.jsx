0f0eab61b543ce0c0851ae655283250101819da1
Hector Daniel Gonzalez Laport <79027049+DangoYT@users.noreply.github.com>
2024-03-01 21:35:17 -0300

    Estilos

--- frontend/src/components/Cupido_Musical/index.jsx
+++ frontend/src/components/Cupido_Musical/index.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
-import styles from './styles.module.css';
+import styles from './cupidoMusical.module.css';
 import axios from 'axios';
 import Button from "../botones/BotonGeneral";
 import BotonCorazon from "../botones/BotonCorazon";
