120ebe6bbd873acc6b9b4de7d927a19a9bdebb79
Hector Daniel Gonzalez Laport <79027049+DangoYT@users.noreply.github.com>
2024-03-02 22:03:00 -0300

    Nose que es jaja

--- frontend/src/components/Cupido_Musical/index.jsx
+++ frontend/src/components/Cupido_Musical/index.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
-import styles from './styles.module.css';
+import styles from './cupidoMusica.module.css';
 import axios from 'axios';
 import Button from "../botones/BotonGeneral";
 import BotonCorazon from "../botones/BotonCorazon";
